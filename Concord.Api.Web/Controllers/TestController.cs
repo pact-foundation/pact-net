8d8d8ef2c1033197199f3415fb8d55bdae1bd06f
Neil Campbell <ncampbell@seek.com.au>
2014-06-30 10:47:04 +1000

    Adding a test api for building the concord functionality

---
+++ Concord.Api.Web/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Concord.Api.Web.Controllers
+{
+    public class TestController : ApiController
+    {
+        [Route("test/helloworld")]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ Concord.Api.Web/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Concord.Api.Web.Controllers;
+using DotNetDoodle.Owin;
+using DotNetDoodle.Owin.Dependencies.Autofac;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup("ApiConfiguration", typeof(Concord.Api.Web.Startup))]
+namespace Concord.Api.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            var builder = new ContainerBuilder();
+            
+            builder.RegisterApiControllers(typeof(TestController).Assembly);
+            builder.RegisterOwinApplicationContainer();
+            var container = builder.Build();
+
+            app.UseAutofacContainer(container).UseWebApiWithContainer(config);
+        }
+    }
+}
