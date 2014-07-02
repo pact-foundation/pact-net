68af49edabdefa6548ce3ef7c852a1aca3fd9441
Neil Campbell <ncampbell@seek.com.au>
2014-07-02 17:47:00 +1000

    Moving the sample projects into a Samples folder

--- PactNet/Properties/AssemblyInfo.cs
+++ PactNet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Concord")]
+[assembly: AssemblyTitle("PactNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Concord")]
+[assembly: AssemblyProduct("PactNet")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
