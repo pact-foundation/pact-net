db06c5fb7dccb31732e3599358cc5b57c6558a9e
Neil Campbell <ncampbell@seek.com.au>
2014-07-11 12:05:46 +1000

    Adding tests for HttpVerb to HttpMethod map

---
+++ PactNet.Tests/Mocks/MockHttpService/Mappers/HttpMethodMapperTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net.Http;
+using PactNet.Mocks.MockHttpService.Mappers;
+using PactNet.Mocks.MockHttpService.Models;
+using Xunit;
+
+namespace PactNet.Tests.Mocks.MockHttpService.Mappers
+{
+    public class HttpMethodMapperTests
+    {
+        public HttpMethodMapper GetSubject()
+        {
+            return new HttpMethodMapper();
+        }
+
+        [Fact]
+        public void Convert_WithHttpVerbThatDoesNotHaveAMap_ThrowsArgumentException()
+        {
+            var mapper = GetSubject();
+
+            Assert.Throws<ArgumentException>(() => mapper.Convert((HttpVerb)10000));
+        }
+
+        [Fact]
+        public void Convert_WithGetHttpVerb_ReturnsGetHttpMethod()
+        {
+            var mapper = GetSubject();
+
+            var httpMethod = mapper.Convert(HttpVerb.Get);
+
+            Assert.Equal(HttpMethod.Get, httpMethod);
+        }
+
+        [Fact]
+        public void Convert_WithPostHttpVerb_ReturnsPostHttpMethod()
+        {
+            var mapper = GetSubject();
+
+            var httpMethod = mapper.Convert(HttpVerb.Post);
+
+            Assert.Equal(HttpMethod.Post, httpMethod);
+        }
+
+
+        [Fact]
+        public void Convert_WithPutHttpVerb_ReturnsPutHttpMethod()
+        {
+            var mapper = GetSubject();
+
+            var httpMethod = mapper.Convert(HttpVerb.Put);
+
+            Assert.Equal(HttpMethod.Put, httpMethod);
+        }
+
+        [Fact]
+        public void Convert_WithDeleteHttpVerb_ReturnsDeleteHttpMethod()
+        {
+            var mapper = GetSubject();
+
+            var httpMethod = mapper.Convert(HttpVerb.Delete);
+
+            Assert.Equal(HttpMethod.Delete, httpMethod);
+        }
+
+        [Fact]
+        public void Convert_WithHeadHttpVerb_ReturnsHeadHttpMethod()
+        {
+            var mapper = GetSubject();
+
+            var httpMethod = mapper.Convert(HttpVerb.Head);
+
+            Assert.Equal(HttpMethod.Head, httpMethod);
+        }
+
+
+        [Fact]
+        public void Convert_WithPatchHttpVerb_ReturnsPatchHttpMethod()
+        {
+            var mapper = GetSubject();
+
+            var httpMethod = mapper.Convert(HttpVerb.Patch);
+
+            Assert.Equal("PATCH", httpMethod.ToString());
+        }
+    }
+}
