f40388f996788348aa62af650b5740dba1ccf899
PK <karthi.email@gmail.com>
2014-07-24 02:09:02 +1000

    clean ups

--- PactNet/NancyContextExtensions.cs
+++ PactNet/NancyContextExtensions.cs
@@ -5,28 +5,31 @@ namespace PactNet
 {
     public static class NancyContextExtensions
     {
+        const string MockRequest = "MockRequest";
+        const string MockResponse = "MockResponse";
+
         public static PactProviderServiceRequest GetMockRequest(this NancyContext context)
         {
-            return context.Items.ContainsKey("MockRequest")
-                ? context.Items["MockRequest"] as PactProviderServiceRequest
+            return context.Items.ContainsKey(MockRequest)
+                ? context.Items[MockRequest] as PactProviderServiceRequest
                 : null;
         }
 
         public static PactProviderServiceResponse GetMockResponse(this NancyContext context)
         {
-            return context.Items.ContainsKey("MockResponse")
-                    ? context.Items["MockResponse"] as PactProviderServiceResponse 
+            return context.Items.ContainsKey(MockResponse)
+                    ? context.Items[MockResponse] as PactProviderServiceResponse 
                     : null;
         }
 
         public static void SetMockRequest(this NancyContext context, PactProviderServiceRequest mockRequest)
         {
-            context.Items["MockRequest"] = mockRequest;
+            context.Items[MockRequest] = mockRequest;
         }
 
         public static void SetMockResponse(this NancyContext context, PactProviderServiceResponse mockResponse)
         {
-            context.Items["MockResponse"] = mockResponse;
+            context.Items[MockResponse] = mockResponse;
         }
     }
 }
