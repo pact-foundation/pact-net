0e94785a2756be1b67ae298717abcfe7cf065fca
Neil Campbell <neil@codecise.com>
2014-10-11 23:14:23 +1100

    Tweaking build config to make build version nicer

---
+++ Build/Setup-Build-Environment.ps1
@@ -0,0 +1,20 @@
+param (
+)
+
+$BuildVersion = $env:APPVEYOR_BUILD_VERSION
+$BuildNumber = $env:APPVEYOR_BUILD_NUMBER
+
+$Version = $BuildVersion -replace ".$BuildNumber", ''
+$AssemblyVersion = $Version -replace "[^0-9,.]", ''
+
+
+$env:PACTNET_ASSEMBLY_VERSION = "$AssemblyVersion"
+
+if($env:APPVEYOR_REPO_TAG)
+{
+	$env:PACTNET_VERSION = "$Version"
+}
+else
+{
+	$env:PACTNET_VERSION = "$BuildVersion"
+}
