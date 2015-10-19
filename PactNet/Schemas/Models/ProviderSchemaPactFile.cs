718baeb83ac26191400aebca992a0f3f4b9c8655
bdarougheh <bdarougheh@seek.com.au>
2015-10-19 16:08:20 +1100

    - Make ProviderSchemaPactFile as public. We use this to generate the pact file.

--- PactNet/Schemas/Models/ProviderSchemaPactFile.cs
+++ PactNet/Schemas/Models/ProviderSchemaPactFile.cs
@@ -4,7 +4,7 @@ using PactNet.Models;
 
 namespace PactNet.Schemas.Models
 {
-    class ProviderSchemaPactFile : PactFile
+    public class ProviderSchemaPactFile : PactFile
     {
         [JsonProperty(PropertyName = "schemas")]
         public IEnumerable<ProviderDataSchema> Schemas { get; set; }
