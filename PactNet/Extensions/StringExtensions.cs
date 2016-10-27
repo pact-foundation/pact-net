0c781e18e396b53d7b713edeff8d2b9a4df7f39f
niing <nguyen.nguyen@rockend.com.au>
2016-10-27 12:09:29 +1100

    Added an adhoc solution to cater for regex-validation

--- PactNet/Extensions/StringExtensions.cs
+++ PactNet/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace PactNet.Extensions
 {
@@ -6,9 +8,33 @@ namespace PactNet.Extensions
     {
         public static string ToLowerSnakeCase(this string input)
         {
-            return !String.IsNullOrEmpty(input) ?
-                input.Replace(' ', '_').ToLower() :
-                String.Empty;
+            return !string.IsNullOrEmpty(input)
+                ? input.Replace(' ', '_').ToLower()
+                : string.Empty;
+        }
+
+        public static Func<string> ToFunc(this string expressionStr)
+        {
+            try {
+                var split = expressionStr?.Split(new[] { "=>" }, StringSplitOptions.None);
+
+                if (split?.Length != 2)
+                    return null;
+
+                var value = split.LastOrDefault()?.TrimStart().TrimEnd();
+
+                if (string.IsNullOrEmpty(value))
+                    return null;
+
+                var expression = Expression.Convert(Expression.Constant(value), typeof(string));
+
+                return Expression
+                    .Lambda<Func<string>>(expression)
+                    .Compile();
+            }
+            catch {
+                return null;
+            }
         }
     }
 }
