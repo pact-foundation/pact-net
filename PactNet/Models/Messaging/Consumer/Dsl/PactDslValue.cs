6bd9b78227e214acaa7fc636bfd9ca76bbd0db23
Chris Newham <chris.newham@philips.com>
2017-01-23 18:46:37 -0500

    Handling array matchers. Good enough for now

--- PactNet/Models/Messaging/Consumer/Dsl/PactDslValue.cs
+++ PactNet/Models/Messaging/Consumer/Dsl/PactDslValue.cs
@@ -56,7 +56,12 @@ namespace PactNet.Models.Messaging.Consumer.Dsl
             var result = new MatcherResult();
 
             foreach (var matcher in _matchers.Values)
-                result.Add(matcher.Match(this.Path, JToken.FromObject(this.Value), message.SelectToken(this.Path)));
+            {
+                var tokens = message.SelectTokens(this.Path);
+
+                foreach (var token in tokens)
+                    result.Add(matcher.Match(token.Path, JToken.FromObject(this.Value), token));
+            }
 
             return result;
         }
