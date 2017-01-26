de4e691eecd6ce94d2d8eb899d59edfbb0d28e21
Hatok, Jerome <jerry.hatok@philips.com>
2017-01-26 09:43:04 -0500

    Moved the Provider.Messaging example into the correct folder structure.

---
+++ Samples/EventApi/Provider.Messaging/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provider.Messaging.Models
+{
+    public class Event
+    {
+        public Guid EventId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string EventType { get; set; }
+        public Location Location { get; set; }
+    }
+}
