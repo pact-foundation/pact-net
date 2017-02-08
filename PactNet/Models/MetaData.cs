970521015671edb00c436c3052e8ddd1d7845c41
Jerry Hatok <jerry.hatok@philips.com>
2017-02-08 11:58:57 -0500

    Changes to get the pact file meta data to match the serialzation of the pact-jvm.

---
+++ PactNet/Models/MetaData.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using PactNet.Models;
+
+namespace PactNet.Models
+{
+    public class MetaData
+    {
+        public MetaData()
+        {
+            PactSpecification = new PactSpecification();
+            PactNet = new pactNet();
+        }
+
+        [JsonProperty(PropertyName = "pact-specification")]
+        public PactSpecification PactSpecification { get; set; }
+        [JsonProperty(PropertyName = "pact-net")]
+        public pactNet PactNet { get; set; }
+    }
+}
--- PactNet/Models/PactFile.cs
+++ PactNet/Models/PactFile.cs
@@ -1,18 +1,24 @@
 ﻿using Newtonsoft.Json;
+using System.Reflection;
 
 namespace PactNet.Models
 {
     public class PactFile : PactDetails
     {
+       
+
         [JsonProperty(PropertyName = "metadata")]
         public dynamic Metadata { get; private set; }
 
         public PactFile()
         {
-            Metadata = new
+            MetaData metaData = new MetaData()
             {
-                pactSpecificationVersion = "1.1.0"
+                PactSpecification = new PactSpecification() { Version = "3.0.0" },
+                PactNet = new pactNet() { Version = Assembly.GetExecutingAssembly().GetName().Version.ToString() }
             };
+
+            Metadata = metaData;
         }
     }
 }
