7cbd87bf6205e79630fb8cfc1a11358de7e3eaff
Neil Campbell <neil@codecise.com>
2017-07-11 19:35:10 +1000

    Adding basics for the pact broker publisher

---
+++ PactNet/PactPublisher.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using PactNet.Configuration.Json;
+using PactNet.Extensions;
+using PactNet.Models;
+
+namespace PactNet
+{
+    public class PactPublisher
+    {
+        private readonly HttpClient _httpClient;
+
+        internal PactPublisher(
+            Uri baseUri,
+            PactUriOptions brokerUriOptions,
+            HttpMessageHandler handler)
+        {
+            //TODO: Handle basic auth stuff
+            _httpClient = new HttpClient(handler) { BaseAddress = baseUri };
+        }
+
+        public PactPublisher(Uri brokerBaseUri, PactUriOptions brokerUriOptions = null) : 
+            this(brokerBaseUri, brokerUriOptions, new HttpClientHandler())
+        {
+        }
+
+        public void PublishToBroker(string pactFileUri, string consumerVersion, IEnumerable<string> tags = null)
+        {
+            if (String.IsNullOrEmpty(pactFileUri))
+            {
+                throw new ArgumentNullException("pactFileUri is null or empty");
+            }
+
+            if (String.IsNullOrEmpty(consumerVersion))
+            {
+                throw new ArgumentNullException("consumerVersion is null or empty");
+            }
+
+            var pactFileText = File.ReadAllText(pactFileUri);
+            var pactFile = JsonConvert.DeserializeObject<PactFile>(pactFileText);
+
+            var request = new HttpRequestMessage(HttpMethod.Put, $"/pacts/provider/{pactFile.Provider.Name}/consumer/{pactFile.Consumer.Name}/version/{consumerVersion}");
+            var requestContentJson = JsonConvert.SerializeObject(request, JsonConfig.ApiSerializerSettings);
+            request.Content = new StringContent(requestContentJson, Encoding.UTF8, "application/json");
+
+            var response = _httpClient.SendAsync(request, CancellationToken.None).RunSync();
+            var responseStatusCode = response.StatusCode;
+            var responseContent = String.Empty;
+
+            Dispose(request);
+            Dispose(response);
+
+            if (response.Content != null)
+            {
+                responseContent = response.Content.ReadAsStringAsync().RunSync();
+            }
+
+            if (responseStatusCode != HttpStatusCode.OK)
+            {
+                throw new PactFailureException($"Failed to publish Pact to the broker: {responseContent}");
+            }
+
+            if (tags != null && tags.Any())
+            {
+                foreach (var tag in tags)
+                {
+                    var tagRequest = new HttpRequestMessage(HttpMethod.Put, $"/pacticipants/{pactFile.Consumer.Name}/versions/{consumerVersion}/tags/{tag}");
+
+                    var tagResponse = _httpClient.SendAsync(tagRequest, CancellationToken.None).RunSync();
+                    var tagResponseStatusCode = tagResponse.StatusCode;
+                    var tagResponseContent = String.Empty;
+
+                    if (tagResponse.Content != null)
+                    {
+                        tagResponseContent = tagResponse.Content.ReadAsStringAsync().RunSync();
+                    }
+
+                    Dispose(tagRequest);
+                    Dispose(tagResponse);
+
+                    if (tagResponseStatusCode != HttpStatusCode.OK)
+                    {
+                        throw new PactFailureException($"Failed to add Pact tag '{tag}' to the broker: {tagResponseContent}");
+                    }
+                }
+            }
+        }
+
+        private void Dispose(IDisposable disposable)
+        {
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
