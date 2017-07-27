25e2e292d90c83fc0385b27cc23a11cf5f61db26
Neil Campbell <neil@codecise.com>
2017-07-27 15:40:52 +1000

    Fixing System.Net.Http dependency for net46

--- Samples/EventApi/Consumer.Tests/EventsApiConsumerTests.cs
+++ Samples/EventApi/Consumer.Tests/EventsApiConsumerTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using PactNet.Matchers;
 using PactNet.Mocks.MockHttpService;
 using PactNet.Mocks.MockHttpService.Models;
