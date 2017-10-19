5fdaaf2eb87bcc1a39796408c1f1786a8679fd49
Neil Campbell <neil@codecise.com>
2017-10-19 22:35:24 +1100

    Updating to the latest standalone core

--- Build/Download-Standalone-Core.ps1
+++ Build/Download-Standalone-Core.ps1
@@ -26,7 +26,7 @@ New-Item -ItemType directory -Path $OutputPath | Out-Null
 
 Import-Module -Name $7ZipSnapIn
 
-$StandaloneCoreVersion = '1.9.1'
+$StandaloneCoreVersion = '1.11.0'
 $StandaloneCoreDownloadBaseUri = "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v$StandaloneCoreVersion"
 
 # Download and extract the Windows core
