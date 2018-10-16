af7063d95d8b16f6b406fd9abbcede3a6c689914
nirpe <nirpe@payoneer.com>
2018-10-16 11:38:34 +0300

    adding tests class

--- PactNet.Tests/Mocks/MockAmqpService/RabbitMqMockProviderServiceTests.cs
+++ PactNet.Tests/Mocks/MockAmqpService/RabbitMqMockProviderServiceTests.cs
@@ -1,6 +1,31 @@
-﻿namespace PactNet.Tests.Mocks.MockAmqpService
+﻿using PactNet.Mocks.MockAmqpService;
+using Xunit;
+using Xunit.Sdk;
+
+namespace PactNet.Tests.Mocks.MockAmqpService
 {
     public class RabbitMqMockProviderServiceTests
     {
+        private RabbitMqMockProviderService _rabbitMqMockProviderService;
+
+        public RabbitMqMockProviderServiceTests()
+        {
+            _rabbitMqMockProviderService = new RabbitMqMockProviderService();
+        }
+
+        [Fact]
+        public void Given_WithProviderState_SetsProviderState()
+        {
+            //Arrange
+            const string providerState = "My provider state";
+
+            //Act
+            _rabbitMqMockProviderService.Given(providerState)
+                .Given(providerState);
+
+            //Assert
+            _rabbitMqMockProviderService.VerifyPublishing();
+//            _mockedHost.Recieved(1).PublishMessage(null);
+        }
     }
 }
