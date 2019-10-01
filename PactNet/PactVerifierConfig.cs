9407a72e05febce7aeca7b31f5eb1a0385fe2c81
Sergiu Calancea <sergiu.calancea@amdaris.com>
2019-10-01 13:10:30 +0300

    Enable multiple custom headers to be set through PactVerifierConfig

--- PactNet/PactVerifierConfig.cs
+++ PactNet/PactVerifierConfig.cs
@@ -11,7 +11,23 @@ namespace PactNet
 
         public string ProviderVersion { get; set; }
 
-        public KeyValuePair<string, string>? CustomHeader { get; set; }
+        private KeyValuePair<string, string>? _customHeader;
+        public KeyValuePair<string, string>? CustomHeader { // backward compatibility
+            get => this._customHeader;
+            set
+            {
+                if (value != null)
+                {
+                    this.CustomHeaders.Add(value.Value.Key, value.Value.Value);
+                }
+                if (this._customHeader != null)
+                {
+                    this.CustomHeaders.Remove(this._customHeader.Value.Value);
+                }
+                this._customHeader = value;
+            }
+        }
+        public Dictionary<string, string> CustomHeaders { get; set; } = new Dictionary<string, string>();
 
         public bool Verbose { get; set; }
 
