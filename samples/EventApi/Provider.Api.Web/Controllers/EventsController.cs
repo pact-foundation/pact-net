b71420938f8520f85a069c4f50e7b3123b144812
Adam Rodger <adam.rodger@gmail.com>
2021-06-03 10:00:55 +0100

    Normalise casing on root level folder

---
+++ samples/EventApi/Provider.Api.Web/Controllers/EventsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Provider.Api.Web.Models;
+
+namespace Provider.Api.Web.Controllers
+{
+    public class EventsController : ApiController
+    {
+        [Authorize]
+        [Route("events")]
+        public IEnumerable<Event> Get()
+        {
+            return GetAllEventsFromRepo();
+        }
+
+        [Route("events/{id}")]
+        public Event GetById(Guid id)
+        {
+            return GetAllEventsFromRepo().First(x => x.EventId == id);
+        }
+
+        [Route("events")]
+        public IEnumerable<Event> GetByType(string type)
+        {
+            return GetAllEventsFromRepo().Where(x => x.EventType.Equals(type, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        [Route("events")]
+        public HttpResponseMessage Post(Event @event)
+        {
+            if (@event == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+
+        private IEnumerable<Event> GetAllEventsFromRepo()
+        {
+            return new List<Event>
+            {
+                new Event
+                {
+                    EventId = Guid.Parse("45D80D13-D5A2-48D7-8353-CBB4C0EAABF5"),
+                    Timestamp = DateTime.Parse("2014-06-30T01:37:41.0660548"),
+                    EventType = "SearchView"
+                },
+                new Event
+                {
+                    EventId = Guid.Parse("83F9262F-28F1-4703-AB1A-8CFD9E8249C9"),
+                    Timestamp = DateTime.Parse("2014-06-30T01:37:52.2618864"),
+                    EventType = "DetailsView"
+                },
+                new Event
+                {
+                    EventId = Guid.Parse("3E83A96B-2A0C-49B1-9959-26DF23F83AEB"),
+                    Timestamp = DateTime.Parse("2014-06-30T01:38:00.8518952"),
+                    EventType = "SearchView"
+                }
+            };
+        }
+    }
+}
---
+++ samples/EventApi/Provider.Api.Web/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Provider.Api.Web;
+using Provider.Api.Web.Controllers;
+
+[assembly: OwinStartup("ApiConfiguration", typeof(Startup))]
+namespace Provider.Api.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            // Owin Middleware; we use token middleware for requests that require authorization.
+            var oAuthBearerOptions = new OAuthBearerAuthenticationOptions();
+            app.UseOAuthBearerAuthentication(oAuthBearerOptions);
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            json.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            json.SerializerSettings.Formatting = Formatting.None;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            var builder = new ContainerBuilder();
+            
+            builder.RegisterApiControllers(typeof(EventsController).Assembly);
+            var container = builder.Build();
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+        }
+    }
+}
