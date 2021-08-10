52c70e317718d0be8adca60e5f5fc9b07e35e30b
Adam Rodger <adam.rodger@gmail.com>
2021-08-10 20:28:19 +0100

    feat!: Download new combined FFI library

--- build/download-native-libs.sh
+++ build/download-native-libs.sh
@@ -1,61 +1,57 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-MOCK_SERVER_VERSION="0.0.17"
-MOCK_SERVER_BASE_URL="https://github.com/pact-foundation/pact-reference/releases/download/libpact_mock_server_ffi-v$MOCK_SERVER_VERSION"
-
-VERIFIER_VERSION="0.0.4"
-VERIFIER_BASE_URL="https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_ffi-v$VERIFIER_VERSION"
+FFI_VERSION="0.0.0"
+FFI_BASE_URL="https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v$FFI_VERSION"
 
 base_path=$(dirname "$0")
 
 download_native() {
-    base_url="$1"
-    component="$2"
-    file="$3"
-    os="$4"
-    platform="$5"
-    extension="$6"
+    file="$1"
+    os="$2"
+    platform="$3"
+    extension="$4"
 
-    url="$base_url/lib$component-$os-$platform.$extension.gz"
+    # e.g.
+    #   pact_ffi-windows-x86_64.dll.gz
+    #   libpact_ffi-linux-x86_64.so.gz
+    #   libpact_ffi-osx-x86_64.dylib.gz
+    src_file="$file-$os-$platform.$extension.gz"
+    url="$FFI_BASE_URL/$src_file"
     sha="$url.sha256"
 
     path="$base_path/$os/$platform"
+    dest_file="$file.$extension.gz"
     mkdir -p "$path"
 
-    echo "Downloading $component"
-    echo "    Platform: $os/$platform"
-    echo "    File: $file.$extension"
+    echo "Downloading FFI library for $os/$platform"
+    echo "    Destination: $path/$dest_file"
     echo "    URL: $url"
 
     echo -n "    Downloading... "
-    curl --silent -L "$url" -o "$path/$file.$extension.gz"
-    curl --silent -L "$sha" -o "$path/$file.$extension.gz.sha256"
+    curl --silent -L "$url" -o "$path/$dest_file"
+    curl --silent -L "$sha" -o "$path/$dest_file.sha256"
     echo "OK"
 
     echo -n "    Verifying... "
 
     if [[ "$OSTYPE" == "darwin"* ]]; then
         # OSX requires an empty arg passed to -i, but this doesn't work on Lin/Win
-        sed -Ei '' "s|../target/artifacts/.+$|$path/$file.$extension.gz|" "$path/$file.$extension.gz.sha256"
-        shasum -a 256 --check --quiet "$path/$file.$extension.gz.sha256"
+        sed -Ei '' "s|../target/artifacts/.+$|$path/$dest_file|" "$path/$dest_file.sha256"
+        shasum -a 256 --check --quiet "$path/$dest_file.sha256"
     else
-        sed -Ei "s|../target/artifacts/.+$|$path/$file.$extension.gz|" "$path/$file.$extension.gz.sha256"
-        sha256sum --check --quiet "$path/$file.$extension.gz.sha256"
+        sed -Ei "s|../target/artifacts/.+$|$path/$dest_file|" "$path/$dest_file.sha256"
+        sha256sum --check --quiet "$path/$dest_file.sha256"
     fi
 
-    rm "$path/$file.$extension.gz.sha256"
+    rm "$path/$dest_file.sha256"
     echo "OK"
 
     echo -n "    Extracting... "
-    gunzip -f "$path/$file.$extension.gz"
+    gunzip -f "$path/$dest_file"
     echo "OK"
 }
 
-download_native "$MOCK_SERVER_BASE_URL" "pact_mock_server_ffi" "pact_mock_server_ffi" "windows" "x86_64" "dll"
-download_native "$MOCK_SERVER_BASE_URL" "pact_mock_server_ffi" "libpact_mock_server_ffi" "linux" "x86_64" "so"
-download_native "$MOCK_SERVER_BASE_URL" "pact_mock_server_ffi" "pact_mock_server_ffi" "osx" "x86_64" "dylib"
-
-download_native "$VERIFIER_BASE_URL" "pact_verifier_ffi" "pact_verifier_ffi" "windows" "x86_64" "dll"
-download_native "$VERIFIER_BASE_URL" "pact_verifier_ffi" "libpact_verifier_ffi" "linux" "x86_64" "so"
-download_native "$VERIFIER_BASE_URL" "pact_verifier_ffi" "pact_verifier_ffi" "osx" "x86_64" "dylib"
+download_native "pact_ffi" "windows" "x86_64" "dll"
+download_native "libpact_ffi" "linux" "x86_64" "so"
+download_native "libpact_ffi" "osx" "x86_64" "dylib"
