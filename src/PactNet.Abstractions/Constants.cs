1e0c1513cbc950ece12dcf2d07d068b74dc4a9e5
Adam Rodger <adam.rodger@gmail.com>
2022-02-15 18:43:30 +0000

    chore: Tidy up some dangling .Net Framework references

--- src/PactNet.Abstractions/Constants.cs
+++ src/PactNet.Abstractions/Constants.cs
@@ -5,12 +5,7 @@ namespace PactNet
 {
     internal static class Constants
     {
-#if USE_NET4X
-        public static string BuildDirectory = new Uri(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", ""))).LocalPath;
-        public static string DefaultPactDir = Path.GetFullPath($"{BuildDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}pacts{Path.DirectorySeparatorChar}");
-#else
         public static string BuildDirectory = AppContext.BaseDirectory;
         public static string DefaultPactDir = Path.GetFullPath($"{BuildDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}pacts{Path.DirectorySeparatorChar}");
-#endif
     }
 }
