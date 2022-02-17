0b9a276db8338106280627132ad12051a5881d98
Adam Rodger <adam.rodger@gmail.com>
2022-02-17 15:39:03 +0000

    chore: Simple integration test to check the pact broker source API

--- tests/PactNet.Tests/Verifier/PactVerifierTests.cs
+++ tests/PactNet.Tests/Verifier/PactVerifierTests.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions.Extensions;
 using Moq;
 using Newtonsoft.Json;
 using PactNet.Verifier;
@@ -50,6 +51,91 @@ namespace PactNet.Tests.Verifier
             this.verifier.MessagingProvider("My Producer", settings);
 
             this.mockProvider.Verify(p => p.SetProviderInfo("My Producer", "https", "localhost", 1234, "/pact-messaging/"));
+        }
+
+        public class IntegrationTests : PactVerifierTests
+        {
+            public IntegrationTests(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public void PactBrokerSource()
+            {
+                // arrange
+                this.mockProvider
+                    .Setup(p => p.Initialise())
+                    .Verifiable();
+
+                this.mockProvider
+                    .Setup(p => p.SetProviderInfo("Provider", "http", "example.org", 8080, "/base-path"))
+                    .Verifiable();
+
+                this.mockProvider
+                    .Setup(p => p.AddBrokerSource(new Uri("https://broker.example.org"),
+                                                  "user",
+                                                  "hunter2",
+                                                  null,
+                                                  true,
+                                                  14.February(2022),
+                                                  new[] { "provider-tag1", "provider-tag2" },
+                                                  "provider-branch",
+                                                  new[]
+                                                  {
+                                                      "{\"deployedOrReleased\":true}",
+                                                      "{\"mainBranch\":true,\"latest\":true}"
+                                                  },
+                                                  new[] { "consumer-tag1", "consumer-tag2" }))
+                    .Verifiable();
+
+                this.mockProvider
+                    .Setup(p => p.SetPublishOptions("1.2.3+abcd123",
+                                                    new Uri("https://ci.example.org/builds/1234"),
+                                                    new[] { "publish-tag1", "publish-tag2" },
+                                                    "provider-publish-branch"))
+                    .Verifiable();
+
+                this.mockProvider
+                    .Setup(p => p.SetProviderState(new Uri("http://example.org:8080/provider-states"), false, true))
+                    .Verifiable();
+
+                this.mockProvider
+                    .Setup(p => p.SetFilterInfo("description", "state", null))
+                    .Verifiable();
+
+                this.mockProvider
+                    .Setup(p => p.SetVerificationOptions(true, 10.Seconds()))
+                    .Verifiable();
+
+                // act
+                this.verifier
+                    .ServiceProvider("Provider", new Uri("http://example.org:8080/base-path"))
+                    .WithPactBrokerSource(new Uri("https://broker.example.org"), options =>
+                     {
+                         options.BasicAuthentication("user", "hunter2")
+                                .ConsumerTags("consumer-tag1", "consumer-tag2")
+                                .ConsumerVersionSelectors(new ConsumerVersionSelector { DeployedOrReleased = true },
+                                                          new ConsumerVersionSelector { MainBranch = true, Latest = true })
+                                .EnablePending()
+                                .IncludeWipPactsSince(14.February(2022))
+                                .ProviderBranch("provider-branch")
+                                .ProviderTags("provider-tag1", "provider-tag2")
+                                .PublishResults("1.2.3+abcd123", publish =>
+                                {
+                                    publish.BuildUri(new Uri("https://ci.example.org/builds/1234"))
+                                           .ProviderBranch("provider-publish-branch")
+                                           .ProviderTags("publish-tag1", "publish-tag2");
+                                });
+                     })
+                    .WithProviderStateUrl(new Uri("http://example.org:8080/provider-states"))
+                    .WithFilter("description", "state")
+                    .WithRequestTimeout(10.Seconds())
+                    .WithSslVerificationDisabled()
+                    .Verify();
+
+                // assert
+                this.mockProvider.Verify();
+            }
         }
     }
 }
