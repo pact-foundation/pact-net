42f52ff798f99693f46df3a3a4497655ffdd2f80
Adam Rodger <adam.rodger@gmail.com>
2022-05-26 20:36:29 +0100

    feat: Allow publishing verification results for URI sources (#392)

--- src/PactNet.Abstractions/Verifier/IPactUriOptions.cs
+++ src/PactNet.Abstractions/Verifier/IPactUriOptions.cs
@@ -23,7 +23,14 @@ namespace PactNet.Verifier
         IPactUriOptions TokenAuthentication(string token);
 
         /// <summary>
-        /// Publish results to the pact broker
+        /// Publish results to the pact broker without any additional settings
+        /// </summary>
+        /// <param name="providerVersion">Provider version</param>
+        /// <returns>Fluent builder</returns>
+        IPactUriOptions PublishResults(string providerVersion);
+
+        /// <summary>
+        /// Publish results to the pact broker with additional settings such as provider branch
         /// </summary>
         /// <param name="providerVersion">Provider version</param>
         /// <param name="configure">Configure the publish options</param>
@@ -31,7 +38,15 @@ namespace PactNet.Verifier
         IPactUriOptions PublishResults(string providerVersion, Action<IPactBrokerPublishOptions> configure);
 
         /// <summary>
-        /// Publish results to the pact broker if the condition is met
+        /// Publish results to the pact broker without any additional settings, if the condition is met
+        /// </summary>
+        /// <param name="condition">Only publish if this condition is true</param>
+        /// <param name="providerVersion">Provider version</param>
+        /// <returns>Fluent builder</returns>
+        IPactUriOptions PublishResults(bool condition, string providerVersion);
+
+        /// <summary>
+        /// Publish results to the pact broker with additional settings such as provider branch, if the condition is met
         /// </summary>
         /// <param name="condition">Only publish if this condition is true</param>
         /// <param name="providerVersion">Provider version</param>
--- tests/PactNet.Tests/Verifier/PactUriOptionsTests.cs
+++ tests/PactNet.Tests/Verifier/PactUriOptionsTests.cs
@@ -60,34 +60,55 @@ namespace PactNet.Tests.Verifier
         }
 
         [Fact]
-        public void PublishResults_WhenCalled_AddsPublishArgs()
+        public void PublishResults_WithoutExtraSettings_AddsPublishArgs()
         {
-            this.options.PublishResults("1.2.3", _ => { });
-            this.options.Apply();
+            this.mockProvider.Setup(p => p.SetPublishOptions("1.2.3", null, Array.Empty<string>(), null));
 
-            this.mockProvider.Verify(p => p.SetPublishOptions("1.2.3",
-                                                              It.IsAny<Uri>(),
-                                                              It.IsAny<ICollection<string>>(),
-                                                              It.IsAny<string>()));
+            this.options.PublishResults("1.2.3");
+
+            this.mockProvider.Verify();
+        }
+
+        [Fact]
+        public void PublishResults_WithExtraSettings_AddsPublishArgs()
+        {
+            var buildUri = new Uri("https://ci.example.org/build/1");
+            this.mockProvider.Setup(p => p.SetPublishOptions("1.2.3", buildUri, new[] { "tag1", "tag2" }, "branch"));
+
+            this.options.PublishResults("1.2.3", settings => settings.ProviderBranch("branch")
+                                                                     .ProviderTags("tag1", "tag2")
+                                                                     .BuildUri(buildUri));
+
+            this.mockProvider.Verify();
         }
 
         [Fact]
         public void PublishResults_ConditionMet_AddsPublishArgs()
         {
-            this.options.PublishResults(true, "1.2.3", _ => { });
-            this.options.Apply();
+            this.mockProvider.Setup(p => p.SetPublishOptions("1.2.3", null, Array.Empty<string>(), null));
 
-            this.mockProvider.Verify(p => p.SetPublishOptions("1.2.3",
-                                                              It.IsAny<Uri>(),
-                                                              It.IsAny<ICollection<string>>(),
-                                                              It.IsAny<string>()));
+            this.options.PublishResults(true, "1.2.3");
+
+            this.mockProvider.Verify();
+        }
+
+        [Fact]
+        public void PublishResults_ConditionMet_AddsAdditionalOptions()
+        {
+            var buildUri = new Uri("https://ci.example.org/build/1");
+            this.mockProvider.Setup(p => p.SetPublishOptions("1.2.3", buildUri, new[] { "tag1", "tag2" }, "branch"));
+
+            this.options.PublishResults(true, "1.2.3", settings => settings.ProviderBranch("branch")
+                                                                           .ProviderTags("tag1", "tag2")
+                                                                           .BuildUri(buildUri));
+
+            this.mockProvider.Verify();
         }
 
         [Fact]
         public void PublishResults_ConditionNotMet_DoesNotAddPublishArgs()
         {
             this.options.PublishResults(false, null, null);
-            this.options.Apply();
 
             this.mockProvider.Verify(p => p.SetPublishOptions(It.IsAny<string>(),
                                                               It.IsAny<Uri>(),
