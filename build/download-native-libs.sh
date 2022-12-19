2b4005fa20d72d54532b2006dcfe18a09041efee
Leonardo Santana <leonardonsantana@gmail.com>
2022-12-19 18:05:47 -0500

    chore: Upgrade FFI to version 0.3.18

--- build/download-native-libs.sh
+++ build/download-native-libs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-FFI_VERSION="0.3.11"
+FFI_VERSION="0.3.18"
 FFI_BASE_URL="https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v$FFI_VERSION"
 
 GREEN="\e[32m"
